41de068dc16b529396cd490ba3b79c7e2db7afbf
Genji Noguchi <genji.noguchi@gmail.com>
2014-09-22 11:03:07 -0400

    String work assignment

---
+++ basic_syntax/work1_strings/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strheaders.h>
+
+
+int main(){
+  
+
+
+
+
+}
+
+int stringLength
---
+++ basic_syntax/work1_strings/strheaders.h
@@ -0,0 +1,3 @@
+
+
+int stringLength(string[]);
